a1fb4637c34cd2bf905b36b2189f3d1b0e7e731f
RajasKhambayate <105958008+RajasKhambayate@users.noreply.github.com>
2023-07-09 03:02:26 +0530

    Add files via upload

---
+++ Programs/OpenFile1.c
@@ -0,0 +1,27 @@
+//This program opens a file in read mode only
+#include<stdio.h>
+#include<fcntl.h>
+
+int	main(int argc,char *argv[])
+{
+	int fd = 0;
+
+	if(argc != 2)
+	{
+		printf("Invalid number of arguments\n");
+		return -1;
+	}
+
+	fd = open(argv[1],O_RDONLY);
+	if(fd == -1)
+	{
+		printf("Unable to open the file\n");
+		return -1;
+	}
+	else
+	{
+		printf("File is successfully opened with FD %d\n",fd);
+	}
+
+	return 0;
+}
