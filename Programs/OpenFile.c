faf3dea088421514e7428ba68dd1521b6a572ce0
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-09 15:59:23 +0530

    This program opens a file in read and write mode .

---
+++ Programs/OpenFile.c
@@ -0,0 +1,33 @@
+//This program opens a file in read and write mode .
+#include<stdio.h>
+#include<fcntl.h>
+
+int	main(int argc,char *argv[])
+{
+	int fd = 0;
+
+	if(argc != 2)
+	{
+		printf("Invalid number of arguments\n");
+		return -1;
+	}
+
+	fd = open(argv[1],O_RDWR);
+	if(fd == -1)
+	{
+		printf("Unable to open the file\n");
+		return -1;
+	}
+	else
+	{
+		printf("File is successfully opened with FD %d\n",fd);
+	}
+
+	return 0;
+}
+/*
+O_RDWR   - Read and Write mode .
+O_RDONLY - Read mode .
+O_WRONLY - Write mode .
+O_CREAT  - Create mode .
+*/
