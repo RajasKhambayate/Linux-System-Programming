ea9b4237ebe37b032ba76e7d931fc0f2d141f348
RajasKhambayate <105958008+RajasKhambayate@users.noreply.github.com>
2023-07-11 23:18:08 +0530

    This program will write data from a file on console using a system call

--- Programs/WriteFile.c
+++ Programs/WriteFile.c
@@ -17,7 +17,7 @@ int main(int argc,char *argv[])
         return -1;
     }
 
-    fd = open(argv[1],O_RDONLY);
+    fd = open(argv[1],O_RDWR);
     if(fd == -1)
     {
         printf("Unable to open file\n");
