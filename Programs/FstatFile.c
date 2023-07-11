c734e11b4f3da65cff332d6bc0f7b505dcb5c1b7
RajasKhambayate <105958008+RajasKhambayate@users.noreply.github.com>
2023-07-12 00:06:07 +0530

    This program displays properties of a file from its file descriptor which currently is running

--- Programs/FstatFile.c
+++ Programs/FstatFile.c
@@ -1,12 +1,12 @@
-//This program performs task of fstat command which displays properties of running file.
+//This program displays properties of a file from its file descriptor which currently is running
+//Usage : "./Name_of_Executable   Name_of_File"
 #include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
 #include<stdlib.h>
-#include<string.h>
+#include<fcntl.h>
+#include<unistd.h>
 #include<sys/stat.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
     if(argc != 2)
     {
@@ -14,18 +14,18 @@ int main(int argc,char *argv[])
         return -1;
     }
 
-    struct stat fstat_obj;
+    struct stat Fstat_obj;
+    int fd = 0;
 
-    int fd = open(argv[1],O_RDONLY);
-    stat(fd,&fstat_obj);
+    fd = open(argv[1],O_RDONLY);
+    fstat(fd,&Fstat_obj);
 
-    printf("File name          : %s\n",argv[1]);
-    printf("File size          : %d\n",fstat_obj.st_size);
-    printf("Number of links    : %d\n",fstat_obj.st_nlink);
-    printf("Inode number       : %d\n",fstat_obj.st_ino);
-    printf("File system number : %d\n",fstat_obj.st_dev);
-    printf("Number of blocks   : %d\n",fstat_obj.st_blocks);
-    printf("Block size         : %d\n",fstat_obj.st_blksize);
+    printf("File name : %s\n",argv[1]);
+    printf("File size is : %d\n",Fstat_obj.st_size);
+    printf("Number of links : %d\n",Fstat_obj.st_nlink);
+    printf("Inode number : %d\n",Fstat_obj.st_ino);
+    printf("File system number : %d\n",Fstat_obj.st_dev);
+    printf("Number of blocks : %d\n",Fstat_obj.st_blocks);
 
     return 0;
 }
