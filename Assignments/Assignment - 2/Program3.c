3378a51d94399b4b5428e74a19e13e2f252e0c03
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-13 20:12:56 +0530

    Code which accepts directory name from user and prints all file names from that directory

---
+++ Assignments/Assignment - 2/Program3.c
@@ -0,0 +1,37 @@
+//Problem : Write a program which accept directory name from user and print all file names from that directory
+//Usage : "./Name_of_Executable   Name_of_Directory"
+#include<stdio.h>
+#include<dirent.h>
+
+int main(int argc,char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent * entry = NULL;
+
+    if(argc != 2)
+    {
+        printf("Insufficient arguments\n");
+        return -1;
+    }
+
+    dp = opendir(argv[1]);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory\n");
+        return -1;
+    }
+
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("Entries from the directory file are : \n");
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("Filenames :\n\n");
+    while((entry = readdir(dp)) != NULL)
+    {
+        printf("%s\n",entry->d_name);
+    }
+    printf("-------------------------------------------------------------------------------------------------\n");
+
+    closedir(dp);
+
+    return 0;
+}
