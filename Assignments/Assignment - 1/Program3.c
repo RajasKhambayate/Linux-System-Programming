878c68fd982a5b817559c00b81e6d8567169f568
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-15 11:15:19 +0530

    Code which accepts file name and mode and checks whether our process can access that file in accepted or not

--- Assignments/Assignment - 1/Program3.c
+++ Assignments/Assignment - 1/Program3.c
@@ -1,8 +1,8 @@
 //Problem : Write a program which accept file name and mode and check whether our process can access that file in accepted or not
 //Usage : "./Name_of_Executable   Name_of_File   Mode_of_Opening_File"
 #include<stdio.h>
-#include<fcntl.h>
 #include<string.h>
+#include<unistd.h>
 
 int	main(int argc,char *argv[])
 {
@@ -14,45 +14,43 @@ int	main(int argc,char *argv[])
 
 	int fd = 0;
 
-
 	if(strcmp(argv[2],"Read") == 0)
 	{
-        fd = open(argv[1],O_RDONLY);
+        fd = access(argv[1],R_OK);
         if(fd == -1)
         {
-            printf("Unable to open the file\n");
+            printf("File can't be accessed in read mode\n");
             return -1;
         }
-        else
+        else if(fd == 0)
         {
-            printf("File is successfully opened with FD : %d in read mode\n",fd);
+            printf("File can be accessed in read mode\n");
         }
-    
     }
 	else if(strcmp(argv[2],"Write") == 0)
 	{
-        fd = open(argv[1],O_WRONLY);
+        fd = access(argv[1],W_OK);
         if(fd == -1)
         {
-            printf("Unable to open the file\n");
+            printf("File can't be accessed in write mode\n");
             return -1;
         }
-        else
+        else if(fd == 0)
         {
-            printf("File is successfully opened with FD : %d in write mode\n",fd);
+            printf("File can be accessed in write mode\n");
         }
 	}
 	else if(strcmp(argv[2],"ReadandWrite") == 0)
 	{
-        fd = open(argv[1],O_RDWR);
+        fd = access(argv[1],R_OK & W_OK);
         if(fd == -1)
         {
-            printf("Unable to open the file\n");
+            printf("File can't be accessed in read & write mode\n");
             return -1;
         }
-        else
+        else if(fd == 0)
         {
-            printf("File is successfully opened with FD : %d in read and write mode\n",fd);
+            printf("File can be accessed in read & write mode\n");
         }
 	}
     else
