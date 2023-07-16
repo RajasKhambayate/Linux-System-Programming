3aedb65e23a061f58684c3496e3738b32275d629
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-16 15:43:39 +0530

    This program writes data from a file on console which is stored in a subdirectory

--- Programs/WriteFile_SubDir.c
+++ Programs/WriteFile_SubDir.c
@@ -1,71 +1,42 @@
 //This program writes data from a file on console which is stored in a subdirectory
-//Usage : "./Name_of_Executable   Name_of_Directory   Name_of_File"
+//Usage : "./Name_of_Executable   Name_of_Directory   Name_of_File Data   Data"
 #include<stdio.h>
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
-#include<dirent.h>
 #include<string.h>
-#include<sys/stat.h>
 
-#define BLOCKSIZE 1024
+#define Block 1024
 
 int main(int argc,char *argv[])
 {
-    if(argc != 3)
+    if(argc != 4)
     {
         printf("Insufficient arguments\n");
         return -1;
     }
 
-    DIR *dp = NULL;
-    struct dirent * entry = NULL;
+    int fd = 0;
+    int iRet = 0;
+    char FilePath[50] = {'\0'};
+    char Buffer[Block] = {'\0'};
 
-    dp = opendir(argv[1]);
-    if(dp == NULL)
+    strcat(Buffer,argv[3]);
+
+    int iNo = snprintf(FilePath,50,"%s/%s",argv[1],argv[2]);
+
+    fd = open(FilePath,O_WRONLY);
+    if(fd == -1)
     {
-        printf("Unable to open directory\n");
+        printf("Unable to open file\n");
         return -1;
     }
 
-    while((entry = readdir(dp)) != NULL)
-    {
-        if(strcmp(argv[2],entry->d_name) == 0)
-        {
-            int fd = 0;
-            int iRet = 0;
-            char FilePath[50] = {'\0'};
-            char Buffer[BLOCKSIZE] = {'\0'};
+    write(fd,Buffer,iRet);
 
-            int iNo = snprintf(FilePath,50,"%s/%s",argv[1],entry->d_name);
+    printf("Data has been written successfully into the file\n");
 
-            fd = open(FilePath,O_RDWR);
-            if(fd == -1)
-            {
-                printf("Unable to open file\n");
-                return -1;
-            }
-
-            while((iRet = read(fd,Buffer,sizeof(Buffer))) != 0)
-            {
-                write(1,Buffer,iRet);
-            }
-
-            close(fd);
-
-            break;
-        }
-        else
-        {
-            continue;
-        }
-    }
-    if((entry = readdir(dp)) == NULL)
-    {
-        printf("File with name '%s' doesn't exist in directory named %s\n",argv[2],argv[1]);
-    }
-
-    closedir(dp);
+    close(fd);
 
     return 0;
 }
