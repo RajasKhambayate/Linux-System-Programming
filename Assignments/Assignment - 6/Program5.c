14d34db40966303f4446c38d8b91761f90f06cef
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-17 00:07:20 +0530

    Code which accepts file name and offset from user , removes all data from that offset

---
+++ Assignments/Assignment - 6/Program5.c
@@ -0,0 +1,38 @@
+//Problem : Write a program which accept file name and offset from user , remove all data from that offset
+//Usage : "./Name_of_Executable   Name_of_File   No_of_Offset"
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Invalid arguments\n");
+        return -1;
+    }
+
+    int fd = 0;
+    int iRet = 0;
+
+    fd = open(argv[1],O_RDWR);
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+
+    iRet = ftruncate(fd,atoi(argv[2]));//truncate(filename,offset);
+    if(iRet == 0)
+    {
+        printf("Data successfully removed\n");
+    }
+    else
+    {
+        printf("Unable to remove data\n");
+        return -1;
+    }
+
+    return 0;
+}
