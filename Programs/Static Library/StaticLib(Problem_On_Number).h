cbf6fb110572bb83c1e6e244190c25177e84a3ed
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-20 22:56:41 +0530

    This file contains methods prototypes from StatLib(Problem_On_Number).c

---
+++ Programs/Static Library/StaticLib(Problem_On_Number).h
@@ -0,0 +1,8 @@
+//This file contains methods prototypes from StatLib(Problem_On_Number).c
+
+void ChkEven(int);
+void ChkOdd(int);
+void ChkPrime(int);
+void DisplayFactors(int);
+void ChkPerfect(int);
+void CountFactors(int);
