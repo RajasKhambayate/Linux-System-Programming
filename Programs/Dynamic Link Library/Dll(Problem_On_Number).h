304041224a62e624defe521c02dd4d55c43f74fe
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-25 23:23:35 +0530

    Modified

--- Programs/Dynamic Link Library/Dll(Problem_On_Number).h
+++
@@ -1,9 +0,0 @@
-//This header file contains the methods prototypes of topic "Problem on number" .
-#include<stdio.h>
-
-void ChkEven(int);
-void ChkOdd(int);
-void ChkPrime(int);
-void DisplayFactors(int);
-void ChkPerfect(int);
-void CountFactors(int);
