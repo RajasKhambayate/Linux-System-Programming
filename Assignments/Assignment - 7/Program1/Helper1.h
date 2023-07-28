31a4f9c424ba5b1266ceaaa0bcb3c37c96c491ac
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-28 12:07:17 +0530

    This program contains prototypes of methods which are used by Program1.c and Dll1.c

---
+++ Assignments/Assignment - 7/Program1/Helper1.h
@@ -0,0 +1,10 @@
+//This program contains prototypes of methods which are used by Program1.c and Dll1.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<dlfcn.h>
+
+void Addition(int,int);
+void Substraction(int,int);
+void Multiplication(int,int);
+void Division(int,int);
