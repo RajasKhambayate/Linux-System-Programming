f8a6d5c985e198b26803a13bcc6deb98422d56a0
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-28 12:10:51 +0530

    This program contains prototypes of methods which are used by Program3.c , Dll3_1.c and Dll3_2.c

---
+++ Assignments/Assignment - 7/Program3/Helper3.h
@@ -0,0 +1,6 @@
+//This program contains prototypes of methods which are used by Program3.c , Dll3_1.c and Dll3_2.c
+#include<stdio.h>
+#include<dlfcn.h>
+
+void PrimeNumber(int);
+void PerfectNumber(int);
