e438e0bd3fee0d4730ebff2d6354cec58e670a0b
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-28 12:13:08 +0530

    This program contains prototypes of methods which are used by Program4.c , Dll4_1.c and Dll4_2.c

---
+++ Assignments/Assignment - 7/Program4/Helper4.h
@@ -0,0 +1,6 @@
+//This program contains prototypes of methods which are used by Program4.c , Dll4_1.c and Dll4_2.c
+#include<stdio.h>
+#include<dlfcn.h>
+
+void Factorial(int);
+void Load_library(int);
