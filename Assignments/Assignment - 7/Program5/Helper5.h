e5b3d6899c08a87aa2254ba5185f24174d833f7d
Rajas Khambayate <rajaskhambayate8@gmail.com>
2023-07-28 12:14:07 +0530

    This program contains prototypes of methods which are included in Staticlib5.c

---
+++ Assignments/Assignment - 7/Program5/Helper5.h
@@ -0,0 +1,8 @@
+//This program contains prototypes of methods which are included in Staticlib5.c .
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<string.h>
+
+void Stat_File(char[]);
